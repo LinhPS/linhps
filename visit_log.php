cf167eaf435f6f1ecbfc0ab26699ae6475a6b71e
LinhPS <linhps.7994@gmail.com>
2020-02-24 11:35:05 +0700

    wip

--- visit_log.php
+++ visit_log.php
@@ -1,3 +1,16 @@
 <?php
-$message = date('Y/m/d H:i:s') . PHP_EOL;
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    // ip from share internet
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    // ip pass from proxy
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} elseif (!empty($_SERVER['REMOTE_ADDR'])) {
+    $ip = $_SERVER['REMOTE_ADDR'];
+} else {
+    $ip = 'anonymous';
+}
+
+$message = date('Y/m/d H:i:s') . ' ' . $ip . PHP_EOL;
+
 file_put_contents('visit_log.txt', $message, FILE_APPEND | LOCK_EX);
